ec5caa4d51eb0ca7b3652ddb56ce873024377ce7
MCLakshan <mlakshancharitha@gmail.com>
2023-09-15 00:28:14 +0530

    Added the checkboxes to the login

---
+++ views/lecturer.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecturer Dashboard</title>
+    <!-- You can include any additional CSS or stylesheets here -->
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Success!</h1>
+        <h2>This the lecturer dashboard.</h2>
+        <a href="../index.php">Back to Home</a>
+    </div>
+</body>
+</html>
