df882c398118f67dd8f515d5bece397c4be9d95d
Charith Lakshan <wgclakshan@gmail.com>
2023-10-31 23:14:01 +0530

    Created Instructor Dashboard

---
+++ app/views/pages/Instructor_dashboard.php
@@ -0,0 +1,7 @@
+<?php $style = "Lecturer_dashboard"; ?>
+
+<?php require APPROOT . '/views/includes/LecturerHeader.php'; ?>
+
+<h1>Instructor Dashboard </h1>
+
+<?php require APPROOT . '/views/includes/LecturerFooter.php'; ?>
