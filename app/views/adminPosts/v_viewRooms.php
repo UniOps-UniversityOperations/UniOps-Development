afbc396dfbaaa6727a57056bf451dda0dbc40467
MCLakshan <mlakshancharitha@gmail.com>
2023-12-03 23:09:58 +0530

    Modified the code

--- app/views/adminPosts/v_viewRooms.php
+++ app/views/adminPosts/v_viewRooms.php
@@ -5,6 +5,13 @@
 
 <h1>View Lecture Rooms / Laboratories / Meeting Rooms</h1>
 
+<div class="search-container">
+    <label for="search">Search:</label>
+    <input type="text" id="search" placeholder="Enter room name...">
+    <button id="searchButton">Search</button>
+</div>
+
+
 <div class="create_room_button">
     <a href="<?php echo URLROOT;?>/AdminPosts/createRoom">
         <button class="create_button">Create Room</button>
@@ -45,30 +52,16 @@
 
 <?php foreach($data['posts'] as $post) : ?>
 
-
     <div class="lecture_room">
         <div class="lecture_room_header">
             <h2><?php echo $post->name; ?> (<?php echo $post->type; ?> room)</h2>
 
             <div class="action_buttons">
-                <button class="view_button">View</button></a>
+                <button class="view_button">View</button>
                 <a href="<?php echo URLROOT; ?>/AdminPosts/updateRoom/<?php echo $post->id ?>"><button class="update_button">Update</button></a>
                 <a href="<?php echo URLROOT; ?>/AdminPosts/deleteRoom/<?php echo $post->id ?>"><button class="delete_button">Delete</button></a>
             </div>
-        </div>
-
-        <!--
-        <div class="lecture_room_body_single_row">
-            <p><b>ID / Code:</b> <?php echo $post->id; ?></p>
-            <p><b>Type:</b> <?php echo $post->type; ?></p>
-            <p><b>Capacity:</b> <?php echo $post->capacity; ?></p>
-            <p><b>Availability:</b> <?php echo $post->current_availability; ?></p>
-        </div>
-    </div>
-    -->
-
-
-    
+        </div>    
 
     <!-- Idle view -->
     <div class="idle-view">
@@ -115,6 +108,9 @@
     </div>
     </div>
 
+    
+<?php endforeach; ?>
+
     <script>
     document.addEventListener('DOMContentLoaded', function () {
         var viewButtons = document.querySelectorAll('.view_button');
@@ -132,48 +128,6 @@
         });
     });
     </script>
-
-
-
-
-<!--
-    <div class="lecture_room">
-        <div class="lecture_room_header">
-            <h1><?php echo $post->name; ?> (<?php echo $post->type; ?> room)</h1>
-
-            <div class="action_buttons">
-                <a href="<?php echo URLROOT;?>/AdminPosts/updateRoom/<?php echo $post->id ?>"><button class="update_button">Update</button></a>
-                <a href="<?php echo URLROOT;?>/AdminPosts/deleteRoom/<?php echo $post->id ?>"><button class="delete_button">Delete</button></a>
-            </div>
-
-        </div>
-        <div class="lecture_room_body">
-            <div class="lecture_room_body_left">
-                <p>ID / Code: <?php echo $post->id; ?></p>
-                <p>Type: <?php echo $post->type; ?></p>
-                <p>Capacity: <?php echo $post->capacity; ?></p>
-                <p>Current Availability: <?php echo $post->current_availability; ?></p>
-                <p>No of Tables: <?php echo $post->no_of_tables; ?></p>
-                <p>No of Chairs: <?php echo $post->no_of_chairs; ?></p>
-                <p>No of Boards: <?php echo $post->no_of_boards; ?></p>
-                <p>No of Projectors: <?php echo $post->no_of_projectors; ?></p>
-                <p>No of Computers: <?php echo $post->no_of_computers; ?></p>
-                <p>Is AC: <?php echo $post->is_ac; ?></p>
-                <p>Is Wifi: <?php echo $post->is_wifi; ?></p>
-                <p>Is Media: <?php echo $post->is_media; ?></p>
-            </div>
-            <div class="lecture_room_body_right">
-                <p>Is Lecture: <?php echo $post->is_lecture; ?></p>
-                <p>Is Lab: <?php echo $post->is_lab; ?></p>
-                <p>Is Tutorial: <?php echo $post->is_tutorial; ?></p>
-                <p>Is Meeting: <?php echo $post->is_meeting; ?></p>
-                <p>Is Seminar: <?php echo $post->is_seminar; ?></p>
-                <p>Is Exam: <?php echo $post->is_exam; ?></p>
-            </div>
-        </div>
-    </div>
--->
-<?php endforeach; ?>
 
 
 <?php require APPROOT . '/views/includes/adminFooter.php'; ?>
