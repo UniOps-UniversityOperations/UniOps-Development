c93c795970cfced877f0f5545c9a3368e3a4ec97
M.C.LAKSHAN <mlakshancharitha@gmail.com>
2024-04-28 10:52:52 +0530

    Ctreated the verall report template

---
+++ app/views/reports/admin/v_OverallReport.php
@@ -0,0 +1,8 @@
+<?php $style = "OverallReport"; ?>
+
+<?php require APPROOT . '/views/includes/adminHeader.php'; ?>
+
+ggs
+
+<!-- Footer Section -->
+<?php require APPROOT . '/views/includes/adminFooter.php'; ?>
