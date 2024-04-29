06aca1395231971e478b34e5d6619ad488a84a47
vishwanthieherath <vishwanthieherath@gmail.com>
2024-04-29 17:32:08 +0530

    update styles

--- app/bootloader.php
+++ app/bootloader.php
@@ -1,8 +1,24 @@
-<!--The initial file that is loaded by the web server.-->
-<!--This can redirect any other file, or load a template file.-->
-<!--The entry point for the application.-->
 
 <?php
+
+    // The initial file that is loaded by the web server.
+    // This can redirect any other file, or load a template file.
+    // The entry point for the application.
+
+    // spl_autoload_register(function($class_name)
+    // {
+    //     $exceptions = [];
+
+    //     if(in_array($class_name, $exceptions)) {
+    //         $file_path = "../app/controllers" . $class_name . ".php";
+    //     } else {
+    //         $file_path = "../app/models/timetables" . $class_name . ".model.php";
+    //     }
+
+    //     if(file_exists($file_path)) {
+    //         require $file_path;
+    //     }
+    // });
 
     // Load Helpers
     require_once 'helpers/URL_Helper.php';
@@ -11,6 +27,7 @@
     // Load Config
     require_once 'config/config.php';
 
+    // Libraries
     require_once 'libraries/Core.php';
     require_once 'libraries/Controller.php';
     require_once 'libraries/Database.php';
